710112b5d3142e88c8f6f8465cb68cafa450f57c
Shinobu Izumi <stagesp1@gmail.com>
2016-09-04 15:34:48 +0000

    set Debugkit on

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -215,7 +215,7 @@ Configure::write('Auth.authenticate.Form.fields.username', 'email');
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
-    // Plugin::load('DebugKit', ['bootstrap' => tr
+    Plugin::load('DebugKit', ['bootstrap' => true]);
 }
 
 /**
