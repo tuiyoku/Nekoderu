cfecc37eda6fca88ff71a2f19b2ee92e0552a3eb
keisuke shingaki <kei4eva4@gmail.com>
2016-11-10 12:16:45 +0900

    お問合せのvalidation＆設定を外部化

--- app/src/Controller/PolicyController.php
+++ app/src/Controller/PolicyController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Controller\AppController;
 use Cake\Event\Event;
 use Cake\Mailer\Email;
+use Cake\Validation\Validator;
 
 /**
  * Users Controller
@@ -18,15 +19,43 @@ class PolicyController extends AppController
         $this->Auth->allow(['index', 'policy', 'encourage', 'contact']);
     }
 
-    public function index(){
+    public function index()
+    {
         $this->viewBuilder()->layout("nekoderu");
     }
 
     public function contact()
     {
         if ($this->request->is('post') && $this->request->data) {
-            $this->sendContact($this->request->data);
-            $this->Flash->success('お問い合わせを受け付けました。');
+
+            $validator = new Validator();
+            $validator
+                ->validatePresence('email')
+                ->notEmpty('name', 'メールアドレスは必須です。')
+                ->add('email', 'validFormat', [
+                    'rule' => 'email',
+                    'message' => '正しいメールアドレスを入力してください。'
+                ])
+                ->validatePresence('name')
+                ->notEmpty('name', 'お名前は必須です。')
+                ->validatePresence('subject')
+                ->notEmpty('subject', 'ご用件は必須です。')
+                ->validatePresence('body')
+                ->notEmpty('body', 'お問い合わせ内容は必須です。');
+
+            $errors = $validator->errors($this->request->data);
+            if (!empty($errors)) {
+                $this->sendContact($this->request->data);
+                $this->Flash->success('お問い合わせを受け付けました。');
+            } else {
+
+                $errorMessages = [];
+                array_walk_recursive($errors, function ($a) use (&$errorMessages) {
+                    $errorMessages[] = $a;
+                });
+
+                $this->Flash->error('入力内容に不備があります。', ['params' => ['errors' => $errorMessages]]);
+            }
         }
         $this->viewBuilder()->layout("nekoderu");
     }
@@ -37,16 +66,18 @@ class PolicyController extends AppController
         $email->viewVars($content);
         $email->template("contact", "contact");
         $email->subject("Nekoderuにお問合せがありました");
-        $email->to(['stagesp1+nekoderu@gmail.com', 'kei4eva4@gmail.com']);
+        $email->to(env('CONTACT_TO'));
         $email->send();
     }
-    
-    public function policy(){
+
+    public function policy()
+    {
         $this->viewBuilder()->layout("plain");
     }
-    
-     public function encourage(){
-        
+
+    public function encourage()
+    {
+
         $this->viewBuilder()->layout("plain");
     }
 }
--- app/src/Template/Element/Flash/error.ctp
+++ app/src/Template/Element/Flash/error.ctp
@@ -1,1 +1,9 @@
 <div class="message error" onclick="this.classList.add('hidden');"><?= h($message) ?></div>
+<?php if (isset($params) AND isset($params['errors'])) : ?>
+    <ul class="collection with-header">
+        <li class="collection-header"><h5><?= __('下記の内容を確認してください。') ?></h5></li>
+        <?php foreach ($params['errors'] as $error) : ?>
+            <li class="collection-item"><i class="material-icons">error</i><?= h($error) ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
